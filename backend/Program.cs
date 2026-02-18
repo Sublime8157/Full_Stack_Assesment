6bd78d749998485b3811690839a2bc8c3e01787d
Joven <nevojnarim@gmail.com>
2026-02-18 19:18:29 +0800

    Configure database setup

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,14 +4,27 @@ using TaskManager.Data;
 DotNetEnv.Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var database = Environment.GetEnvironmentVariable("DB_NAME");
+var username = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASS");
 
+if (string.IsNullOrEmpty(host) ||
+    string.IsNullOrEmpty(username) ||
+    string.IsNullOrEmpty(password) ||
+    string.IsNullOrEmpty(database))
+{
+    throw new Exception("Some database environments are empty.");
+}
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = $"Host={host};Database={database};Username={username};Password={password};";
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
